9210d45cd31a49ba7cb82c6570336ac371e6b1f8
Bob <smartcat007@hotmail.com>
2017-01-05 15:03:10 +0100

    Finished graphical part of tic tac toe

--- main.cpp
+++ main.cpp
@@ -17,13 +17,11 @@ typedef std::vector<std::vector<int>> board_type;
 class command {
     public:
         virtual void execute() = 0;
-
-        virtual void undo() = 0;
 };
 
 
 class game {
-        std::stack<command *> commands;
+        std::vector<command *> commands;
         board_type board{{0, 0, 0},
                          {0, 0, 0},
                          {0, 0, 0}};
@@ -61,7 +59,7 @@ class game {
 
         void place(command *c) {
             c->execute();
-            commands.push(c);
+            commands.push_back(c);
         }
 
         int get_winner(int row, int col) {
@@ -74,15 +72,25 @@ class game {
                 stop();
                 return get_turn();
             }
+            if(std::count_if(board[0].begin(), board[0].end(), [](int i){return i == 1 || i == 2;}) == 3 &&
+                    std::count_if(board[1].begin(), board[1].end(), [](int i){return i == 1 || i == 2;}) == 3  &&
+                    std::count_if(board[2].begin(), board[2].end(), [](int i){return i == 1 || i == 2;}) == 3 ) {
+                stop();
+                return -666;
+            }
             return -1;
         }
 
 
         void undo() {
+            board = {{0, 0, 0},
+                     {0, 0, 0},
+                     {0, 0, 0}};
             if (commands.size() > 0) {
-                command *c = commands.top();
-                c->undo();
-                commands.pop();
+                commands.pop_back();
+                std::for_each(commands.begin(), commands.end(), [](command *c) {
+                    c->execute();
+                });
             }
         }
 
@@ -95,11 +103,18 @@ class game {
         }
 
         void set_board_value(int row, int col, int value) {
-            assert(board[row][col] != value);
+            assert(board[row][col] == 0);
             board[row][col] = value;
             if (get_winner(row, col) > 0) {
                 std::cout << "winner player " << get_turn() << "\n";
+            } else if(get_winner(row, col) == -666) {
+                std::cout << "IT IS A TIE" << std::endl;
             }
+            switch_turn();
+        }
+
+        void set_turn(int t) {
+            turn = t;
         }
 
         void switch_turn() {
@@ -120,27 +135,20 @@ class game {
 };
 
 
-class test_command : public command {
+class place_command : public command {
         int previous_value;
         int previous_turn;
         int row, col;
         game &g;
     public:
-        test_command(game &g, int row, int col) : g(g), row(row), col(col) {
+        place_command(game &g, int row, int col) : g(g), row(row), col(col) {
             previous_value = g.get_board()[row][col];
             previous_turn = g.get_turn();
         };
 
         virtual void execute() {
+            g.set_turn(previous_turn);
             g.set_board_value(row, col, g.get_turn());
-            g.switch_turn();
-            g.print_board();
-        }
-
-        virtual void undo() {
-            g.set_board_value(row, col, previous_value);
-            g.switch_turn();
-            g.print_board();
         }
 };
 
@@ -149,10 +157,9 @@ class test_command : public command {
 int main() {
     game g;
 
-
-    std::string decision;
     g.run();
     if (GRAPHICAL == 0) {
+        std::string decision;
         g.print_board();
         std::cout << "Do a move by typing the position like (0,1) \n type undo to undo your turn0";
         while (g.is_running()) {
@@ -161,19 +168,49 @@ int main() {
             if (decision == "undo") {
                 g.undo();
             } else {
-                g.place(new test_command(g, decision[0] - 48, decision[1] - 48));
+                g.place(new place_command(g, decision[0] - 48, decision[1] - 48));
             }
+            g.print_board();
         }
     } else {
-        sf::RenderWindow window{sf::VideoMode{640, 480}, "Game"};
-        sf::vector<
+        auto const grid_size = 100;
+        sf::Texture X, O, BG;
+        if (!X.loadFromFile("assets/x.png") || !O.loadFromFile("assets/o.png") ||
+            !BG.loadFromFile("assets/bg.jpg")) { return -1; }
+        sf::Sprite bg_sprite;
+        bg_sprite.setTexture(BG);
+        sf::RenderWindow window{sf::VideoMode{grid_size * 3, grid_size * 3}, "Game"};
         while (window.isOpen()) {
-            sf::Event event;
-            while (window.pollEvent(event)) {
-                if (event.type == sf::Event::Closed)
+            sf::Event e;
+            while (window.pollEvent(e)) {
+                if ((e.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+                    || e.type == sf::Event::Closed
+                        ) {
                     window.close();
+                } else if (e.type == sf::Event::MouseButtonPressed && g.is_running()) {
+                    sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                    g.place(new place_command(g, mousePosition.x / grid_size, mousePosition.y / grid_size));
+                } else if (e.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+                    g.undo();
+                } else if (e.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
+                    g.run();
+                }
+
             }
             window.clear();
+            window.draw(bg_sprite);
+            for (int i = 0; i < g.get_board().size(); i++) {
+                for (int j = 0; j < g.get_board().size(); j++) {
+                    sf::Sprite sprite;
+                    if (g.get_board()[i][j] == 1) {
+                        sprite.setTexture(X);
+                    } else if (g.get_board()[i][j] == 2) {
+                        sprite.setTexture(O);
+                    }
+                    sprite.setPosition(i * grid_size, j * grid_size);
+                    window.draw(sprite);
+                }
+            }
             window.display();
         }
     }
